26be96fb659a2a41ad58ce84a98ad55fd99000d2
wesleyeduardo.dev <140851807+wesleyeduardodev2023@users.noreply.github.com>
2023-08-11 15:16:26 -0300

    inserindo lista de tecnicos manual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,50 @@
 import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatButtonModule} from "@angular/material/button";
 import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
 import {NavComponent} from './components/nav/nav.component';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatListModule} from "@angular/material/list";
-import {MatIconModule} from "@angular/material/icon";
-import {MatTooltipModule} from "@angular/material/tooltip";
 import {HomeComponent} from './components/home/home.component';
-import {MatCardModule} from "@angular/material/card";
 import {HeaderComponent} from './components/header/header.component';
 import {TecnicoListComponent} from './components/tecnico/tecnico-list/tecnico-list.component';
-import {MatTableModule} from "@angular/material/table";
-import {MatPaginatorModule} from "@angular/material/paginator";
+
+import {MatTooltipModule} from "@angular/material/tooltip";
 import {MatSortModule} from "@angular/material/sort";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 
 const materialModules = [
-  MatToolbarModule,
-  MatButtonModule,
-  MatSidenavModule,
-  MatListModule,
-  MatIconModule,
   MatTooltipModule,
-  MatCardModule,
-  MatTableModule,
+  MatSortModule,
+  MatFormFieldModule,
   MatPaginatorModule,
-  MatSortModule
+  MatSnackBarModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatTableModule,
+  MatRadioModule,
+  MatInputModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule
 ];
+
 
 @NgModule({
   declarations: [
