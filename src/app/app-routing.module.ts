3dc2b5fa93e986fa337702ea3d98fed9423b0d3b
wesleyeduardo.dev <140851807+wesleyeduardodev2023@users.noreply.github.com>
2023-08-12 15:55:43 -0300

    iniciando testes de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {NavComponent} from "./components/nav/nav.component";
 import {HomeComponent} from "./components/home/home.component";
 import {TecnicoListComponent} from "./components/tecnico/tecnico-list/tecnico-list.component";
 import {LoginComponent} from "./components/login/login.component";
+import {AuthGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
+  { path: 'login', component: LoginComponent },
   {
-    path: '', component: NavComponent, children: [
-      {path: 'home', component: HomeComponent},
-      {path: 'tecnicos', component: TecnicoListComponent}
+    path: '', component: NavComponent, canActivate: [AuthGuard], children: [
+      { path: 'home', component: HomeComponent },
+
+      { path: 'tecnicos',            component:   TecnicoListComponent }
     ]
   }
 ];
